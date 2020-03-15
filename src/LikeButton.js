d612bb428fb6ee82541bf1e20aade430cb0a93f8
Rob W <robpwatkins@gmail.com>
2020-03-14 19:15:44 -0500

    updated so liked beers only renders when like button is clicked

--- src/LikeButton.js
+++ src/LikeButton.js
@@ -1,36 +1,25 @@
 import React from 'react';
 import './App.css';
+import LikedBeers from './LikedBeers'
 
 export default class LikeButton extends React.Component {
   state = {
     isLiked: false,
-    buttonVisible: false
   }
 
-  likeBeer = (props) => {
+  likeBeer = () => {
     this.setState({
       isLiked: true
     })
     this.props.addthebeer(this.props.beername)
-    this.setState({ buttonVisible: true })
-  }
-
-  showLikedBeers = (props) => {
-    console.log(this.props.likedbeers);
   }
 
   render() {
     return (
       <div>
         <button onClick={ () => this.likeBeer() }>{ this.state.isLiked ? 'Liked!' : 'Like' }</button>
-        <div className={ this.state.buttonVisible ? '' : 'hidden' } onClick={ () => this.showLikedBeers() }
-        >Liked Beers:
-          {this.props.likedbeers.map(likedBeer => {
-            return (
-            <h6 key={likedBeer}>{likedBeer}</h6>
-            )
-          })
-          }</div>
+        <div>{ this.state.isLiked ? <LikedBeers likedbeers={ this.props.likedbeers }/> : '' }
+        </div>
       </div>
     )
   }
---
+++ src/LikedBeers.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+export default function LikedBeers(props) {
+  return (
+    <div className="likedBeers">
+      <h5>Liked Beers:</h5>
+      <div>
+        {props.likedbeers.map(likedBeer => {
+          return (
+            <h6 key={ likedBeer }>{ likedBeer }</h6>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
